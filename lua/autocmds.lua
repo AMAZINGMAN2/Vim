a9d4435ef0287c21e7b9f2f04ece912607c5f80e
AMAZINGMAN2 <hashim.almuqbel@gmail.com>
2026-07-14 21:30:35 +0300

    updated README.md

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,11 +1,14 @@
-vim.api.nvim_create_autocmd("BufReadPost", {
+vim.api.nvim_create_autocmd("BufEnter", {
   callback = function()
     local mark = vim.api.nvim_buf_get_mark(0, '"')
     local line = mark[1]
     local col = mark[2]
 
     if line > 0 and line <= vim.api.nvim_buf_line_count(0) then
-      vim.api.nvim_win_set_cursor(0, { line, col })
+      vim.schedule(function()
+        vim.api.nvim_win_set_cursor(0, { line, col })
+        vim.cmd("normal! zz")
+      end)
     end
   end,
 })
